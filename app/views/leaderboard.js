c0a345201f3aa831449a045b9e934b967e57c682
Benedykt Roszko <benroszko@gmail.com>
2020-02-02 00:50:35 +0100

    Add nav-tabs

--- app/views/leaderboard.js
+++ app/views/leaderboard.js
@@ -1,15 +1,41 @@
 import { loadScores } from './dbConnector.js';
 
-const scores = [];
-
-(function loadFromDb() {
-	loadScores()
-		.then((data) => scores.push(data))
-		.then((bruh) => console.log(scores))
-		.then((kek) => console.log(getScoresByLevel(scores, 'EASY')));
-})();
+async function loadFromDb() {
+	return await loadScores();
+}
 
 function getScoresByLevel(arr, lvl) {
-	return arr.forEach((score) => console.log(score.name));
-	// .sort((a, b) => a.time < b.time);
+	return arr.filter((score) => score.level === lvl).sort((a, b) => a.time < b.time);
 }
+
+function displayScores(scores) {
+	const emptyTableP = document.getElementById('empty-table-case');
+
+	if (scores.length === 0) {
+		emptyTableP.style.display = 'block';
+	} else {
+		emptyTableP.style.display = 'none';
+		//TODO: display table-rows
+	}
+}
+
+function changeViewOnNavTabClick(scores) {
+	const navTabs = [ ...document.getElementsByClassName('nav-link') ];
+	navTabs.forEach((tab) => {
+		tab.addEventListener('click', () => {
+			const activeOne = navTabs.find((nav) => nav.classList.contains('active'));
+			if (tab !== activeOne) {
+				activeOne.classList.remove('active');
+				tab.classList.add('active');
+				const activeScores = getScoresByLevel(scores, tab.textContent.toUpperCase());
+				displayScores(activeScores);
+			}
+		});
+	});
+}
+window.onload = async () => {
+	const scores = await loadFromDb();
+	changeViewOnNavTabClick(scores);
+	const easyScores = getScoresByLevel(scores, 'EASY');
+	displayScores(easyScores);
+};
